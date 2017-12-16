56bc63382be2caf632e824a7c2df237ec1cdb664
Isuru Herath <isuruh@lseg.com>
2017-12-17 01:20:46 +0530

    Footer effect added

--- Solution2/js/contentPage.js
+++ Solution2/js/contentPage.js
@@ -11,28 +11,22 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("div.show-more > a").click(function(){
 
+    
+
         if($("div.show-more > a > div.caption").text()=="MORE PROJECTS") {
             $("div#hiddenProjects").removeClass("hidden");
             $("div#hiddenProjects").show();
             $("div.show-more > a > div.caption").text("SHOW LESS");
             $("div.show-more > a > img").hide();
+            $('.side-bar').height($('#root').height() + 260 +'px');
         } else if( $("div.show-more > a > div.caption").text()=="SHOW LESS"){
             // $("div#hiddenProjects").hide();
             // $("div#hiddenProjects").css("display","none");
             $("div#hiddenProjects").addClass("hidden");
             $("div.show-more > a > div.caption").text("MORE PROJECTS");
             $("div.show-more > a > img").show();
+              $('.side-bar').height($('.content-bar').height() - 260 +'px');
         }
-        $('.side-bar').height($('.content-bar').height());
-        // $("div#hiddenProjects").removeClass("hidden");
-        // $("div#hiddenProjects").show();
 
-
- 
-        // if ($('.side-bar').height() > $('.content-bar').height()) {
-        //     $('.content-bar').height($('.side-bar').height());
-        // } else {
-        //     $('.side-bar').height($('.content-bar').height());
-        // } 
    });
  });
