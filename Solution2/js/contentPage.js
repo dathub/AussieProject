b5fdf8d76486de693e448bc7f37981356b8f6d8b
Dumindu Thilakaratne <dumindut@lseg.com>
2017-12-20 21:32:53 +0530

    Added Content

--- Solution2/js/contentPage.js
+++ Solution2/js/contentPage.js
@@ -1,32 +1,22 @@
 $(document).ready(function(){
-
-    if ($('.side-bar').height() > $('.content-bar').height()) {
-        $('.content-bar').height($('.side-bar').height());
-    } else {
-        $('.side-bar').height($('.content-bar').height());
-    }
-       
+    $('.side-bar').height($('.content-bar').height());
 });
 
-$(document).ready(function(){
-    $("div.show-more > a").click(function(){
+// $(document).ready(function(){
+//     $("div.show-more > a").click(function(){
 
     
 
-        if($("div.show-more > a > div.caption").text()=="MORE PROJECTS") {
-            $("div#hiddenProjects").removeClass("hidden");
-            $("div#hiddenProjects").show();
-            $("div.show-more > a > div.caption").text("SHOW LESS");
-            $("div.show-more > a > img").hide();
-            $('.side-bar').height($('#root').height() + 260 +'px');
-        } else if( $("div.show-more > a > div.caption").text()=="SHOW LESS"){
-            // $("div#hiddenProjects").hide();
-            // $("div#hiddenProjects").css("display","none");
-            $("div#hiddenProjects").addClass("hidden");
-            $("div.show-more > a > div.caption").text("MORE PROJECTS");
-            $("div.show-more > a > img").show();
-              $('.side-bar').height($('.content-bar').height() - 260 +'px');
-        }
+//         if($("div.show-more > a > div.caption").text()=="MORE PROJECTS") {
 
-   });
- });
+//             $("div.show-more > a > div.caption").text("SHOW LESS");
+//             $("div.show-more > a > img").hide();
+//             $('.side-bar').height($('#root').height() + 260 +'px');
+//         } else if( $("div.show-more > a > div.caption").text()=="SHOW LESS"){
+//             $("div.show-more > a > div.caption").text("MORE PROJECTS");
+//             $("div.show-more > a > img").show();
+//               $('.side-bar').height($('.content-bar').height() - 260 +'px');
+//         }
+
+//    });
+//  });
--- Solution2/js/footerReveal.js
+++ Solution2/js/footerReveal.js
@@ -1,13 +1,29 @@
 $(function(){
   $(window).scroll(function() {
     var height = $(document).height() - ($(document).scrollTop() + $(window).height());
-    if((height <= 200))
+    if((height <= 250))
     {    
-      $('#footer').addClass("show");
+      var footerHeight = 250 - height;
+      var risePercentage = ((footerHeight)/$(document).height())*100;
+      var contentPageTop = footerHeight + "px";
+      // $("div#footer > div.row").css({bottom: contentPageTop})
+      $("div#footer").height(footerHeight);
+      $("div#footer > div.row").height(footerHeight);
+      $('.side-bar').height($('.content-bar').height());
+      // $("div.content-page").css({top: contentPageTop});
+      // $("div.content-page > div.row > div.right-col").css({opacity: 0});
+      // $("section#section01").css({position: 'fixed'});
+      // $('#footer').addClass("show");
     }
     else
     {
-      $('#footer').removeClass("show");
+      $('.side-bar').height($('.content-bar').height());
+      $("div#footer").height(0);
+      $("div#footer > div.row").height(0);
+      // $("div.content-page > div.row >  div.right-col").css({opacity: 1});
+      // $('#footer').removeClass("show");
     }
   });
 })
+
+
