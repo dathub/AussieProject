1a9d8732636b3a2b8f5565a102147ed267a07395
Isuru Herath <isuruh@lseg.stockex.local>
2018-01-20 02:42:21 +0530

    Created join our army page

---
+++ Solution1/js/join-our-army.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function(){
+    $(".slide1-btn").click(function(){
+		$("#slide1").hide();
+		$("#left-col1-text").html("good answer");
+	});
+	
+	$(".slide2-btn").click(function(){
+		$("#slide2").hide();
+		$("#left-col1-text").html("watch yo mouth gurl <br/><br/> jk, jk");
+	});
+	
+	$(".slide3-btn").click(function(){
+		$("#slide3").hide();
+		$("#left-col1-text").html("yeah lad");
+	});
+	
+	$(".slide4-btn").click(function(){
+		$("#slide4").hide();
+		$("#left-col1-text").html("hello, its me, guess again");
+	});
+	
+	$(".slide5-btn").click(function(){
+		$("#slide5").hide();
+		$("#left-col1-text").html("yo-ready");
+	});
+});
