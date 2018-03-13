757fcc83e141a51be00a7cd21808966cf188d649
Isuru Herath <isuruh@lseg.stockex.local>
2018-03-14 00:47:49 +0530

    [Feature] Added wait time for join our army slides

--- ResponsiveSolution/js/joinOurArmy.js
+++ ResponsiveSolution/js/joinOurArmy.js
@@ -7,30 +7,57 @@ jQuery(document).ready(function(){
     $(".slide1-btn").click(function(){
 		$("#slide1").hide();
 		$("#slide2").show();
-		$(".descriptions-box > .wrapper-middle > p").html("good answer");
 	});
 	
 	$(".slide2-btn").click(function(){
-		$("#slide2").hide();
-		$("#slide3").show();
-		$(".descriptions-box > .wrapper-middle > p").html("watch yo mouth gurl <br/><br/> jk, jk");
+		$(".descriptions-box > .wrapper-middle > p").html("good answer");
+		setTimeout(slide2transition, 3000);
 	});
 	
 	$(".slide3-btn").click(function(){
-		$("#slide3").hide();
-		$("#slide4").show();
-		$(".descriptions-box > .wrapper-middle > p").html("yeah lad");
+		$(".descriptions-box > .wrapper-middle > p").html("watch yo mouth gurl <br/><br/> jk, jk");
+		setTimeout(slide3transition, 3000);
 	});
 	
 	$(".slide4-btn").click(function(){
-		$("#slide4").hide();
-		$("#slide5").show();
-		$(".descriptions-box > .wrapper-middle > p").html("hello, its me, guess again");
+		$(".descriptions-box > .wrapper-middle > p").html("yeah lad");
+		setTimeout(slide4transition, 3000);
 	});
 	
 	$(".slide5-btn").click(function(){
-		$("#slide5").hide();
-		$("#slide6").show();
+		setTimeout(slide5transition, 3000);
 		$(".descriptions-box > .wrapper-middle > p").html("yo-ready");
 	});
+	
+	$(".wrong").click(function(){
+		$(".descriptions-box > .wrapper-middle > p").html("try again...");
+	});
+	
+	$(".wrong-slide5").click(function(){
+		$(".descriptions-box > .wrapper-middle > p").html("hello, its me, guess again");
+	});
+	
+	function slide2transition() {
+		$("#slide2").hide();
+		$("#slide3").show();
+		$(".descriptions-box > .wrapper-middle > p").html("");
+	}
+	
+	function slide3transition() {
+		$("#slide3").hide();
+		$("#slide4").show();
+		$(".descriptions-box > .wrapper-middle > p").html("");
+	}
+	
+	function slide4transition() {
+		$("#slide4").hide();
+		$("#slide5").show();
+		$(".descriptions-box > .wrapper-middle > p").html("");
+	}
+	
+	function slide5transition() {
+		$("#slide5").hide();
+		$("#slide6").show();
+		$(".descriptions-box > .wrapper-middle > p").html("");
+	}
 });
